beeb2cf68393e494bce791837a2e9b2389324d30
JimmyCookss <daralebo@gmail.com>
2025-03-09 17:13:03 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#Start
+#Ask user to INPUT their name
+#Ask user to INPUT age
+#PRINT name
+#PRINT age
+#PRINT "Hello World"
+#Stop
+
+name = input("Please type in your name: ")
+age = input("Please type in your age: ")
+print (name)
+print (age)
+print("Hello World!")
