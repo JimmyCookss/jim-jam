f07f2e9b11710ef9c35d2e4fc4c6b935ffbe5319
JimmyCookss <daralebo@gmail.com>
2025-03-09 17:18:48 +0200

    Add files via upload

---
+++ L1T02/details.py
@@ -0,0 +1,13 @@
+#Start
+#Ask user to INPUT their name, age, house number, street name
+#Print out their name, age house number and street name on a single line
+#Stop
+
+
+
+name = input("Please type in your name: ")
+age = input("Please type in your age: ")
+house_number = input("Please type in your house number: ")
+street_name = input("Please type in your street name: ")
+
+print("Good day, this is " + name + ". He is " + age + " years old and lives at house number " + house_number + " on " + street_name + ".")
